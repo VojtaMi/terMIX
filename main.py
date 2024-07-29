f2286644d1f022b4942c389e2dd2132e9c12dc66
Vojtech Mikel <tami.vojkel@gmail.com>
2024-07-29 18:45:55 +0200

    begining english version

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    lang = request.args.get('lang', 'en')
+    if lang == 'en':
+        return render_template('en/index.html')
+    return render_template('cz/index.html')
 
 if __name__ == "__main__":
+
     app.run(debug=True)
