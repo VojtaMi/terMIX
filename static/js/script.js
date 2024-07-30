f5620e86b2134b7bb79c05497cd78c40da962496
Vojtech Mikel <tami.vojkel@gmail.com>
2024-07-30 09:38:52 +0200

    active buttons display

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-// script.js
+// Function to highlight the active page link
 document.addEventListener("DOMContentLoaded", function() {
     var navLinks = document.querySelectorAll('.nav-link');
     var currentUrl = window.location.pathname;
@@ -6,6 +6,22 @@ document.addEventListener("DOMContentLoaded", function() {
     navLinks.forEach(function(link) {
         if (link.getAttribute('href') === currentUrl) {
             link.classList.add('active');
+        }
+    });
+});
+
+// Function to highlight the active language link
+document.addEventListener("DOMContentLoaded", function() {
+    var langLinks = document.querySelectorAll('.lang-link');
+    var currentUrlParams = new URLSearchParams(window.location.search);
+    var currentLang = currentUrlParams.get('lang');
+
+    langLinks.forEach(function(link) {
+        var linkLang = new URLSearchParams(link.getAttribute('href').split('?')[1]).get('lang');
+        if (linkLang === currentLang) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
         }
     });
 });
