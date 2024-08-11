bbd9cd991ea7a90cb3abb30e014f87c6bf9f1de6
vojta_mi <tami.vojkel@gmail.com>
2024-08-11 21:41:57 +0200

    gallery animation in javascript, remove jquery

--- static/js/script_gallery.js
+++ static/js/script_gallery.js
@@ -1,17 +1,18 @@
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function () {
     function centerElement(element) {
-        var $element = $(element);
-        if ($element.length) {
-            var elementOffset = $element.offset().top;
-            var elementHeight = $element.outerHeight();
-            var viewportHeight = $(window).height();
+        var el = document.querySelector(element);
+        if (el) {
+            var elementOffset = el.getBoundingClientRect().top + window.pageYOffset;
+            var elementHeight = el.offsetHeight;
+            var viewportHeight = window.innerHeight;
 
             // Calculate the scroll position to center the element
             var scrollTop = elementOffset - (viewportHeight / 2) + (elementHeight / 2);
 
             // Smoothly scroll to the calculated position
-            $('html, body').animate({
-                scrollTop: scrollTop
+            window.scrollTo({
+                top: scrollTop,
+                behavior: 'smooth'
             });
         }
     }
