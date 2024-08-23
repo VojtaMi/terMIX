1f4bfa621a2a4c9e0709c713b538e67e53c56b4d
Vojtech Mikel <tami.vojkel@gmail.com>
2024-08-23 18:14:16 +0200

    toggle buttons for food drinks work correctly - arrow changes direction when clicked

--- app/static/js/script_drink_menu.js
+++ app/static/js/script_drink_menu.js
@@ -9,6 +9,19 @@ document.addEventListener('DOMContentLoaded', function () {
 // Function to style drink button when clicked
 document.querySelectorAll('.button-header').forEach(function(button) {
     button.addEventListener('click', function() {
+        // Toggle the clicked class on the button itself
         this.classList.toggle('clicked');
+
+        // Find the icon inside the button
+        var icon = this.querySelector('.toggle-icon');
+
+        // Toggle the icon class between bi-caret-down and bi-caret-up
+        if (icon.classList.contains('bi-caret-down')) {
+            icon.classList.remove('bi-caret-down');
+            icon.classList.add('bi-caret-up');
+        } else {
+            icon.classList.remove('bi-caret-up');
+            icon.classList.add('bi-caret-down');
+        }
     });
 });
