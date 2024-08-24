1859a61430869aac839efdcfee5d7cc861b7be96
Vojtech Mikel <tami.vojkel@gmail.com>
2024-08-24 19:19:49 +0200

    gallery extra pictures button loads extra pictures

--- app/static/js/script_gallery.js
+++ app/static/js/script_gallery.js
@@ -1,3 +1,4 @@
+// Center the carousel when the page loads
 document.addEventListener("DOMContentLoaded", function () {
     function centerElement(element) {
         var el = document.querySelector(element);
@@ -16,7 +17,22 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         }
     }
-
-    // Center the carousel when the page loads
     centerElement('#myCarousel');
 });
+
+// display extra image section
+document.addEventListener('DOMContentLoaded', function() {
+        // Get the button element
+        var button = document.getElementById('gallery_btn');
+        // Get the hidden image section
+        var hiddenImageSection = document.getElementById('hidden-image-section');
+
+        // Add click event listener to the button
+        button.addEventListener('click', function() {
+            // Remove 'd-none' class to show the hidden image section
+            hiddenImageSection.classList.remove('d-none');
+
+            // Optionally hide the button after clicking
+            button.classList.add('d-none');
+        });
+    });
