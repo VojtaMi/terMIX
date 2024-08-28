5b8bb51898da7fd024b95e2638fd2064b9751225
Vojtech Mikel <tami.vojkel@gmail.com>
2024-08-28 22:23:25 +0200

    keyboard swith between events

--- app/static/js/script_events.js
+++ app/static/js/script_events.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
+        // Select all flip cards
         var flipCards = document.querySelectorAll('.flip-card');
 
+        // Add event listeners for each flip card
         flipCards.forEach(function (card) {
+            // Click event to toggle the flip
             card.addEventListener('click', function () {
                 this.querySelector('.flip-card-inner').classList.toggle('is-flipped');
+            });
+
+            // Keypress event to toggle the flip
+            card.addEventListener('keypress', function (event) {
+                // Check if the Enter key (key code 13) or Space key (key code 32) was pressed
+                if (event.key === 'Enter' || event.key === ' ') {
+                    event.preventDefault(); // Prevent default action (scrolling for Space)
+                    this.querySelector('.flip-card-inner').classList.toggle('is-flipped');
+                }
             });
         });
     });
