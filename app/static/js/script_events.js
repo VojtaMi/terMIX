7e02120a7608eb700f06da7a72015b54ad8bb7c1
Vojtech Mikel <tami.vojkel@gmail.com>
2024-08-28 21:36:04 +0200

    events flip as cards

--- app/static/js/script_events.js
+++ app/static/js/script_events.js
@@ -1,18 +1,9 @@
-window.addEventListener('load', function() {
-  const flipCard = document.getElementById('flipCard');
-  const flipCardImage = document.getElementById('flipCardImage');
+document.addEventListener('DOMContentLoaded', function () {
+        var flipCards = document.querySelectorAll('.flip-card');
 
-  // Ensure the card adjusts to the image size
-  flipCardImage.onload = function() {
-    const cardWidth = flipCardImage.clientWidth;
-    const cardHeight = flipCardImage.clientHeight;
-
-    flipCard.style.width = `${cardWidth}px`;
-    flipCard.style.height = `${cardHeight}px`;
-  };
-
-  // Handle the case when the image might already be cached and loaded
-  if (flipCardImage.complete) {
-    flipCardImage.onload(); // Trigger the resize
-  }
-});
+        flipCards.forEach(function (card) {
+            card.addEventListener('click', function () {
+                this.querySelector('.flip-card-inner').classList.toggle('is-flipped');
+            });
+        });
+    });
